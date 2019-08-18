ab621bf351740c186602891674429a55d66a5dc1
newdecide <liberty-1@nate.com>
2019-08-18 23:30:53 +0900

    Update j001_06_190818_Add.java

--- BaekjoonAlgorithm-master/src/javaalgo/j001_add/j001_06_190818_Add.java
+++ BaekjoonAlgorithm-master/src/javaalgo/j001_add/j001_06_190818_Add.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+
+public class j001_06_190818_Add {
+  // 9085번
+  // 10보다 작거나 같은 자연수 N개를 주면 합을 구하는 프로그램을 작성하시오.
+  
+  // 2
+  // 5
+  // 1 1 1 1 1
+  // 7
+  // 1 2 3 4 5 6 7
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tries;
+        tries = sc.nextInt();
+
+        for (int i = 0; i < tries; i++) {
+            int a = sc.nextInt();
+            int sum = 0;
+            for (int j = 0; j < a; j++) {
+                sum += sc.nextInt();
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
 
