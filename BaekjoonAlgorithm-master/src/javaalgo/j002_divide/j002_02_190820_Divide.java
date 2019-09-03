513b641e6ba6b3e9467515a734b9c45e053087dd
newdecide <libery-1@nate.com>
2019-09-03 22:48:18 +0900

    Divide_03_java

--- BaekjoonAlgorithm-master/src/javaalgo/j002_divide/j002_02_190820_Divide.java
+++ BaekjoonAlgorithm-master/src/javaalgo/j002_divide/j002_02_190820_Divide.java
@@ -1,9 +1,11 @@
+package javaalgo.j002_divide;
+
 import java.util.Scanner;
 
-public class j002_02_190820_devide {
+public class j002_02_190820_Divide {
 
 public static void main(String[] args) {
-		// 1075번
+	// 1075번
     // 두 정수 N과 F가 주어진다. 
     // 지민이는 정수 N의 가장 뒤 두 자리를 적절히 바꿔서 
     // N을 F로 나누어 떨어지게 만들려고 한다. 
