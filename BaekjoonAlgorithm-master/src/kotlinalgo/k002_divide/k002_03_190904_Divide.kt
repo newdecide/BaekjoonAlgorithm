d2c05d8cc40f2ae3a57f700b1b3bc374f5f3c349
newdecide <libery-1@nate.com>
2019-09-04 23:17:49 +0900

    Divide03_kotlin

---
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k002_divide/k002_03_190904_Divide.kt
@@ -0,0 +1,20 @@
+package kotlinalgo.k002_divide
+
+import java.util.*
+
+// 10430
+// 나머지
+// 첫째 줄에 (A+B)%C, 둘째 줄에 (A%C + B%C)%C, 셋째 줄에 (A×B)%C, 넷째 줄에 (A%C × B%C)%C를 출력한다.
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val c = sc.nextInt()
+    sc.close()
+
+    println((a + b) % c)
+    println((a % c + b % c) % c)
+    println(a * b % c)
+    println(a % c * b % c % c)
+}
