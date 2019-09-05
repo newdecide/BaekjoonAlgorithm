52b7bb6877f3417838cfe42ac528d711810a35b3
newdecide <libery-1@nate.com>
2019-09-05 20:33:33 +0900

    input_helloworld_java

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_190905_input_helloworld.java
@@ -0,0 +1,11 @@
+package javaalgo.j014_input;
+
+public class j014_190905_input_helloworld {
+    public static void main(String[] args) {
+        // 2557번
+        // 입력없음
+        // Hello World! 출력
+
+        System.out.println("Hello World!");
+    }
+}
