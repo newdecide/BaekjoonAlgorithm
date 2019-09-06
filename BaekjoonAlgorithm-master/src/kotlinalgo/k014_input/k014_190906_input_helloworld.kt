ac4b6e4e621a7011dbd6883d3c317af8d96847d3
newdecide <liberty-1@nate.com>
2019-09-06 20:03:56 +0900

    input_helloworld_kotlin

--- BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_190906_input_helloworld.kt
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_190906_input_helloworld.kt
@@ -2,8 +2,9 @@ package kotlinalgo.k014_input
 
 fun main(args: Array<String>) {
         // 2557번
-        // 입력없음
-        // Hello World! 출력
+        
+        // 입력값 없음
+        // 출력값 Hello World!
 
         println("Hello World!")
 }
