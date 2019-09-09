ba323a7136e17ff7c804ea5644183f84989fa36e
newdecide <libery-1@nate.com>
2019-09-09 20:30:50 +0900

    input_cat_java

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_03_190909_input_cat.java
@@ -0,0 +1,13 @@
+package javaalgo.j014_input;
+
+public class j014_03_190909_input_cat {
+    // 10171번
+    // 고양이
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
