53cde241931ff159850fcebd30f12874f06118d4
newdecide <libery-1@nate.com>
2019-09-10 20:21:47 +0900

    input_cat

---
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_03_190910_input_cat.kt
@@ -0,0 +1,12 @@
+package kotlinalgo.k014_input
+
+fun main(args: Array<String>) {
+    // 10171번
+    // 고양이
+
+        println("\\    /\\")
+        println(" )  ( ')")
+        println("(  /  )")
+        println(" \\(__)|")
+
+}
