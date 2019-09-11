2365d8471a19c32651d5f92eec21fc6b3cf83000
newdecide <libery-1@nate.com>
2019-09-11 21:47:47 +0900

    input_dog

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_04_190911_input_dog.java
@@ -0,0 +1,12 @@
+package javaalgo.j014_input;
+
+public class j014_04_190911_input_dog {
+    // 10172번
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
