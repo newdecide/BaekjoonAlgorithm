1dbdb60c69672e17c149dd69ffd8517856397335
newdecide <libery-1@nate.com>
2019-09-12 19:48:34 +0900

    input_dog

---
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_04_190912_input_dog.kt
@@ -0,0 +1,11 @@
+package kotlinalgo.k014_input
+
+// 10172번
+
+fun main(args: Array<String>) {
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+}
