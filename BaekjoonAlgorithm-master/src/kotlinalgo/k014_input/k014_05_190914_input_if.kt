b5de415b37c51aa02d34ebfe107081a95076fc44
newdecide <liberty-1@nate.com>
2019-09-14 19:40:38 +0900

    intput_if_kotlin

--- BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_05_190914_input_if.kt
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_05_190914_input_if.kt
@@ -5,6 +5,7 @@ import java.util.*
 fun main(args: Array<String>) {
     // 1330번
     // 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+    
     val sc = Scanner(System.`in`)
 
     val A = sc.nextInt()
