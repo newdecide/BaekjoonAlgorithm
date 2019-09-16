dead91e73d8a403857eef5d5e4762e7117dc9429
newdecide <liberty-1@nate.com>
2019-09-16 17:00:11 +0900

    input_grade_kotlin

--- BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_06_190916_input_grade.kt
+++ BaekjoonAlgorithm-master/src/kotlinalgo/k014_input/k014_06_190916_input_grade.kt
@@ -2,7 +2,16 @@ package kotlinalgo.k014_input
 
 import java.util.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) {    
+     // 9498번
+        
+     // 시험 점수를 입력받아
+     // 90 ~ 100점은 A, 80 ~ 89점은
+     // B, 70 ~ 79점은
+     // C, 60 ~ 69점은
+     // D, 나머지 점수는
+     // F를 출력하는 프로그램을 작성하시오.
+    
     var sc: Scanner = Scanner(System.`in`)
     var n: Int = sc.nextInt();
 
