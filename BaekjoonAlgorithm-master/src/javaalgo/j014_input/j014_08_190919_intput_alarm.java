905982a7afd44731d094a11d6bbfcb49a1d724b7
newdecide <libery-1@nate.com>
2019-09-19 22:09:00 +0900

    input_alarm_java

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_08_190919_intput_alarm.java
@@ -0,0 +1,24 @@
+package javaalgo.j014_input;
+
+import java.util.Scanner;
+
+public class j014_08_190919_intput_alarm {
+    // 2884번
+    // 알람 맞추기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        int ansH = H;
+        int ansM = M - 45;
+        if(ansM < 0){
+            ansM += 60;
+            ansH--;
+            if(ansH < 0){
+                ansH += 24;
+            }
+        }
+        System.out.println(ansH + " " + ansM);
+        sc.close();
+    }
+}
