d5ec5eeee4ad780059614a959e6759c0d2344c19
newdecide <liberty-1@nate.com>
2019-09-24 22:55:49 +0900

    input_forit_kotlin

--- BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_190923_input_forif.java
+++ BaekjoonAlgorithm-master/src/javaalgo/j014_input/j014_190923_input_forif.java
@@ -1,4 +1,26 @@
 package javaalgo.j014_input;
 
+import java.util.Scanner;
+
 public class j014_190923_input_forif {
+    // 10871번
+    // x보다 작은 수
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; ++i) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] < x) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
 }
