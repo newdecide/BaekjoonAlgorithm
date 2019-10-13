1e9ff6c5ecd02d6dd57eda0b0f3475f42da50106
newdecide <liberty-1@nate.com>
2019-10-13 23:23:00 +0900

    Factorial_java

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j006_nsum/j006_04_191013_Factorial.java
@@ -0,0 +1,25 @@
+package javaalgo.j006_nsum;
+
+import java.util.Scanner;
+
+public class j006_04_191013_Factorial {
+    // 10872번 팩토리얼
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+            int res = 1;
+
+            for (int i = a; i >= 1; i--) {
+                res *= i;
+            }
+        System.out.println(res);
+
+    }
+
+
+
+
+}
