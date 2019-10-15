109c9f4bb9f14dabdf73ce320871d085c65857f2
newdecide <liberty-1@nate.com>
2019-10-15 17:08:31 +0900

    Startprint12_java

---
+++ BaekjoonAlgorithm-master/src/javaalgo/j008_starprint/j008_12_191015_Starprint.java
@@ -0,0 +1,70 @@
+package javaalgo.j008_starprint;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 2447번
+//
+public class j008_12_191015_Starprint {
+    private static final Scanner sc = new Scanner(System.in);
+    private static char[][] starMap;
+
+    public static void main(String args[]) {
+        int num = sc.nextInt();
+        starMap = new char[num][num];
+
+        for (int i = 0; i < num; i++) {
+            Arrays.fill(starMap[i], ' ');
+        }
+
+        solve(0, 0, num);
+        for (int i = 0; i < num; i++) {
+            System.out.println(starMap[i]);
+        }
+    }
+
+    public static void solve (int x, int y, int n) {
+        if (n == 1){
+            starMap[x][y] = '*';
+            return;
+        }
+        int m = n/3;
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                if (i==1 && j==1){
+                    continue;
+                }
+                solve(x+m*i, y+m*j, m);
+            }
+        }
+    }
+}
+
+
+// ***************************
+// * ** ** ** ** ** ** ** ** *
+// ***************************
+// ***   ******   ******   ***
+// * *   * ** *   * ** *   * *
+// ***   ******   ******   ***
+// ***************************
+// * ** ** ** ** ** ** ** ** *
+// ***************************
+// *********         *********
+// * ** ** *         * ** ** *
+// *********         *********
+// ***   ***         ***   ***
+// * *   * *         * *   * *
+// ***   ***         ***   ***
+// *********         *********
+// * ** ** *         * ** ** *
+// *********         *********
+// ***************************
+// * ** ** ** ** ** ** ** ** *
+// ***************************
+// ***   ******   ******   ***
+// * *   * ** *   * ** *   * *
+// ***   ******   ******   ***
+// ***************************
+// * ** ** ** ** ** ** ** ** *
+// ***************************
